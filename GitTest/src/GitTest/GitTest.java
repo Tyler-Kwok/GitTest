954c58dadcd9e34f016183e7018bb2e5b3b165f2
Tyler <Tyler@Tyler-PC>
2013-03-20 10:02:59 +1100

    lc

--- GitTest/src/GitTest/GitTest.java
+++ GitTest/src/GitTest/GitTest.java
@@ -2,17 +2,6 @@ package GitTest;
 
 public class GitTest{
 	//clean up
-	//left 2
-	//right 1
-	//left 3
-	//right 2
-	//left 4
-	//left 5
-	//right rebase test
-	//checkin 1
-	//checkin 2
-	//checkin 3
-	//left merge test
-	//right merge test    
+	//local commit
 
 }
