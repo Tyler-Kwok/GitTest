7dd0ac660046ee09e3f2d97f5057a78455a44b34
Tyler <Tyler@Tyler-PC>
2013-03-19 11:01:06 +1100

    clean up

--- GitTest/src/GitTest/GitTest.java
+++ GitTest/src/GitTest/GitTest.java
@@ -1,33 +1,7 @@
 package GitTest;
 
 public class GitTest{
-	// step 1
-	// step 2
-	// commit 1
-	// add step 3
-	// commit 2
-	// add step 4
-	// commit 3
-	// add step 5
-	// commit 4
-	// commit 5a
-	// commit 5
-	// commit 6a
-	// commit 7a
-	// commit 8a
-	// commit 6
-	// commit 7
-	// commit 8
-	// commit 9
-	// commit 10
-	// commit 11
-	// commit 12
-// amended
-	
-	// edit after amended
-	// edit after amended again and again
-	
-// try amend commit again again again  asdsadf
+// clean up?
 	
 
 }
