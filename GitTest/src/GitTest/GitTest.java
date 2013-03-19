cffad50d0dc504a13ee86d43a462510d83e1ae73
Tyler <Tyler@Tyler-PC>
2013-03-19 14:02:33 +1100

    clean left 1

--- GitTest/src/GitTest/GitTest.java
+++ GitTest/src/GitTest/GitTest.java
@@ -1,16 +1,7 @@
 package GitTest;
 
 public class GitTest{
-// clean up?!
-	// change testaaa
-	// do sth
-	// left 1
-	// right update 5
-	// right update 6
-	// right update 7
-	// right update 8 
-	// left 2
-	// right update 9
-	// left 3
+//clean up
+//left 1
 
 }
